e8f04dfdc2e5d53ca0d7b9ba7c7e9c0fdeeb0c11
Eric Tangedal <73506971+DunCentral@users.noreply.github.com>
2022-08-28 15:41:14 -0700

    Added Comments

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,29 @@ package src;
 public class Main {
     public static void main(String[] args){
         for(int i=1; i<101; i++){
+            //If i is a multiple of 3 and a multiple of 5, print "FizzBuzz"
             if(i%3==0 && i%5==0){
                 System.out.println("FizzBuzz");
             }
+            //If i is a multiple of 3, print "Fizz"
             else if(i%3==0){
                 System.out.println("Fizz");
             }
+            //If i is a multiple of 5, print "Buzz"
             else if(i%5==0){
                 System.out.println("Buzz");
             }
-
+            //else, print i
             else {
                 System.out.println(i);
             }
         }
     }
 }
+/*
+Eric Tangedal
+CS113-1971
+Lab1
+Version 1.0
+8/28/2022
+ */
